1ba6d71352cbdf18bf585dc1fe87c53c93c513cc
leetingman <dwa08@naver.com>
2022-05-23 22:28:24 +0800

    test shppingcart service

--- src/test/java/org/kosta/finalproject/SpringBootPotatoMarketApplicationTests.java
+++ src/test/java/org/kosta/finalproject/SpringBootPotatoMarketApplicationTests.java
@@ -3,9 +3,11 @@ package org.kosta.finalproject;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.assertj.core.api.Assert;
 import org.junit.jupiter.api.Test;
 import org.kosta.finalproject.model.domain.ImageVO;
 import org.kosta.finalproject.model.domain.ItemVO;
+import org.kosta.finalproject.model.domain.ShoppingCartVO;
 import org.kosta.finalproject.model.mapper.ItemMapper;
 import org.kosta.finalproject.model.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,6 +45,37 @@ class SpringBootPotatoMarketApplicationTests {
 		System.out.println(imageList);
 
 	}
+	
+	// Insert shopping cart test
+	@Test
+	void shCartInsertTest() {
+//		ShoppingCartVO vo=new ShoppingCartVO(null, 0);
+//		vo.setItemId(1);
+//		vo.setUserId("1234");
+		String userId="1234";
+		int itemId=1;
+		itemService.insertCart(userId, itemId);
+		
+	}
+	
+	//Select Sh list
+	@Test
+	void shCartSelectTest() {
+		String userId="1234";
+		List<ItemVO> list =new ArrayList<ItemVO>();
+		list=
+		itemService.selectCartList(userId);
+		for(int i=0;i<list.size();i++) {
+			System.out.println(list.get(i));
+		}
+//		Assert<Assert<SELF,ACTUAL>, ACTUAL>
+		
+	}
+	
+ 
+	
+	
+	
 	
 
 }
